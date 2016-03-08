9cfdd0890f37ee9db55bf5aa14fd998f179f4a97
DmNox <dmnox6@gmail.com>
2016-03-08 12:17:42 +0300

    commit project

---
+++ remind/src/main/java/dmn/remind/App.java
@@ -0,0 +1,13 @@
+package dmn.remind;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
